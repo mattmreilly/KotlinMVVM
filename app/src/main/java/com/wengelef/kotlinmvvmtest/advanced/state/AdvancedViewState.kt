91546afa20d042288d16f83557c34d23174157d3
Florian Wengelewski <wengelef@gmail.com>
2017-05-22 15:50:23 +0200

    fix : imports

--- app/src/main/java/com/wengelef/kotlinmvvmtest/advanced/state/AdvancedViewState.kt
+++ app/src/main/java/com/wengelef/kotlinmvvmtest/advanced/state/AdvancedViewState.kt
@@ -1,10 +1,12 @@
 package com.wengelef.kotlinmvvmtest.advanced.state
 
+import com.wengelef.kotlinmvvmtest.model.User
+
 sealed class AdvancedViewState : State<AdvancedViewState> {
 
     class Idle: AdvancedViewState()
     class Loading: AdvancedViewState()
-    data class Success(val users: List<com.wengelef.kotlinmvvmtest.model.User>): AdvancedViewState()
+    data class Success(val users: List<User>): AdvancedViewState()
     class Error(val message: String): AdvancedViewState()
     class Connection(val connected: Boolean): AdvancedViewState()
 }
--- app/src/main/java/com/wengelef/kotlinmvvmtest/advanced/state/State.kt
+++ app/src/main/java/com/wengelef/kotlinmvvmtest/advanced/state/State.kt
@@ -4,5 +4,5 @@ interface State<T> {
 
     fun <U> convert(converter: (State<T>) -> State<U>): State<U> = converter(this)
 
-    fun value(): com.wengelef.kotlinmvvmtest.advanced.state.State<T> = this
+    fun value(): State<T> = this
 }
